3747f642eefa5f9db8bed61456d873dcb086bcc8
tolluset <dlqud19@gmail.com>
2025-03-23 06:11:59 +0900

    chore: temp

--- app/app/api/bridge/route.ts
+++ app/app/api/bridge/route.ts
@@ -40,80 +40,80 @@ function extractCommandId(executeData: string): string {
 
 const provider = new JsonRpcProvider("https://rpc.xrplevm.org");
 
-export async function POST(request: Request): Promise<NextResponse> {
-  try {
-    const body = await request.json();
-
-    const interchainTransfer = {
-      // TODO: Check if this is correct
-      messageType: ethers.getBigInt("0"), // 0 - Send transfer
-      tokenId: encodeBytes32String("uxrp"), // XRP token ID
-      sourceAddress: toUtf8Bytes("rneW39mLuntP8RDKFcELba29BYb6U5ZQAG"),
-      destinationAddress: toUtf8Bytes(
-        "0xB8Bb795b364550281feb9037e70E366CEa379290"
-      ),
-      amount: parseUnits(dropsToXrp(body.amount).toString()),
-      data: "0x",
-    };
-
-    const abiCoder = new AbiCoder();
-
-    const messageEncoded = abiCoder.encode(
-      ["uint256", "bytes32", "bytes", "bytes", "uint256", "bytes"],
-      [
-        interchainTransfer.messageType,
-        interchainTransfer.tokenId,
-        interchainTransfer.sourceAddress,
-        interchainTransfer.destinationAddress,
-        interchainTransfer.amount,
-        interchainTransfer.data,
-      ]
-    );
-
-    const hubMessage = abiCoder.encode(
-      ["uint256", "string", "bytes"],
-      // TODO: Check if this is correct
-      [ethers.getBigInt("3"), "xrpl-evm", messageEncoded]
-    );
-
-    const wallet = new Wallet(process.env.PRIVATE_KEY!, provider);
-
-    // const tx = await wallet.sendTransaction({
-    //   from: wallet.address,
-    //   to: "0x48CF6E93C4C1b014F719Db2aeF049AA86A255fE2", // IAxelarExecutable 컨트랙트 주소
-    //   data: "EXECUTE_DATA",
-    //   value: "0",
-    // });
-    // await tx.wait();
-
-    const appContract = new Contract(
-      "0x48CF6E93C4C1b014F719Db2aeF049AA86A255fE2", // IAxelarExecutable 컨트랙트 주소
-      IAxelarExecutable.abi,
-      wallet
-    );
-    const tx = await appContract.execute(
-      ethers.getBigInt("0"), // 0 - Send transfer
-      "axelarnet",
-      "0xB8Bb795b364550281feb9037e70E366CEa379290",
-      hubMessage
-    );
-    const receipt = await tx.wait();
-
-    return NextResponse.json(
-      { success: true, transactionHash: receipt.hash },
-      { status: 200 }
-    );
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      return NextResponse.json({ error: error.message }, { status: 500 });
-    }
-
-    return NextResponse.json(
-      { error: "Internal server error" },
-      { status: 500 }
-    );
-  }
-}
+// export async function POST(request: Request): Promise<NextResponse> {
+//   try {
+//     const body = await request.json();
+//
+//     const interchainTransfer = {
+//       // TODO: Check if this is correct
+//       messageType: ethers.getBigInt("0"), // 0 - Send transfer
+//       tokenId: encodeBytes32String("uxrp"), // XRP token ID
+//       sourceAddress: toUtf8Bytes("rneW39mLuntP8RDKFcELba29BYb6U5ZQAG"),
+//       destinationAddress: toUtf8Bytes(
+//         "0xB8Bb795b364550281feb9037e70E366CEa379290"
+//       ),
+//       amount: parseUnits(dropsToXrp(body.amount).toString()),
+//       data: "0x",
+//     };
+//
+//     const abiCoder = new AbiCoder();
+//
+//     const messageEncoded = abiCoder.encode(
+//       ["uint256", "bytes32", "bytes", "bytes", "uint256", "bytes"],
+//       [
+//         interchainTransfer.messageType,
+//         interchainTransfer.tokenId,
+//         interchainTransfer.sourceAddress,
+//         interchainTransfer.destinationAddress,
+//         interchainTransfer.amount,
+//         interchainTransfer.data,
+//       ]
+//     );
+//
+//     const hubMessage = abiCoder.encode(
+//       ["uint256", "string", "bytes"],
+//       // TODO: Check if this is correct
+//       [ethers.getBigInt("3"), "xrpl-evm", messageEncoded]
+//     );
+//
+//     const wallet = new Wallet(process.env.PRIVATE_KEY!, provider);
+//
+//     // const tx = await wallet.sendTransaction({
+//     //   from: wallet.address,
+//     //   to: "0x48CF6E93C4C1b014F719Db2aeF049AA86A255fE2", // IAxelarExecutable 컨트랙트 주소
+//     //   data: "EXECUTE_DATA",
+//     //   value: "0",
+//     // });
+//     // await tx.wait();
+//
+//     const appContract = new Contract(
+//       "0x48CF6E93C4C1b014F719Db2aeF049AA86A255fE2", // IAxelarExecutable 컨트랙트 주소
+//       IAxelarExecutable.abi,
+//       wallet
+//     );
+//     const tx = await appContract.execute(
+//       ethers.getBigInt("0"), // 0 - Send transfer
+//       "axelarnet",
+//       "0xB8Bb795b364550281feb9037e70E366CEa379290",
+//       hubMessage
+//     );
+//     const receipt = await tx.wait();
+//
+//     return NextResponse.json(
+//       { success: true, transactionHash: receipt.hash },
+//       { status: 200 }
+//     );
+//   } catch (error: unknown) {
+//     if (error instanceof Error) {
+//       return NextResponse.json({ error: error.message }, { status: 500 });
+//     }
+//
+//     return NextResponse.json(
+//       { error: "Internal server error" },
+//       { status: 500 }
+//     );
+//   }
+// }
 
 // health check
 export async function GET(): Promise<NextResponse> {
