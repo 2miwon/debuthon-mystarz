c08293761b129c62a98aeef5d400d59d8f5d238c
tolluset <dlqud19@gmail.com>
2025-03-22 13:50:20 +0900

    feat: change metadata

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'MyStarz',
+  description: '내가 가장 빛이 날 때, 스타와 내가 함께 증명하는 지금',
   generator: 'v0.dev',
 }
 
